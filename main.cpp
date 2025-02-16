ab63cb197c4bae7defa3c057bd8551d1f2cf51d2
YaruG1022 <yaru.gao@wsu.edu>
2025-02-15 16:57:20 -0800

    Configured SFML and created a sample program.

--- main.cpp
+++ main.cpp
@@ -7,9 +7,36 @@
 * 
 */
 
-int main(void) 
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/System.hpp"
+#include "SFML/Audio.hpp"
+#include "SFML/System.hpp"
+
+int main()
 {
+    // Create an SFML window
+    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML Red Circle");
 
-// jason the chillest yn...
+    // Create a red circle
+    sf::CircleShape circle(100.f); // Radius = 100 pixels
+    circle.setFillColor(sf::Color::Red);
+    circle.setPosition(350.f, 350.f); // Centered in the window
 
+    while (window.isOpen())
+    {
+        sf::Event e;
+        while (window.pollEvent(e))
+        {
+            if (e.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Render
+        window.clear(sf::Color::Black); // Clear the screen with black
+        window.draw(circle); // Draw the red circle
+        window.display(); // Display everything
+    }
+
+    return 0;
 }
